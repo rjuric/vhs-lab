f47c4cae0104f874660f52af8af2e3c66ea38cdf
rjuric <juricroko@protonmail.com>
2024-07-11 22:34:22 +0200

    initialize project

---
+++ src/main/java/com/rjuric/vhs_lab/VhsLabApplication.java
@@ -0,0 +1,13 @@
+package com.rjuric.vhs_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjuric/vhs_lab/VhsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjuric.vhs_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
