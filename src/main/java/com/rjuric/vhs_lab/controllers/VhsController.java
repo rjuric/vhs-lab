e8247f4ded0c66cf720df57a2b8ebc018e15af12
rjuric <juricroko@protonmail.com>
2024-07-11 23:17:44 +0200

    add basic CRUD controller methods

---
+++ src/main/java/com/rjuric/vhs_lab/controllers/VhsController.java
@@ -0,0 +1,38 @@
+package com.rjuric.vhs_lab.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/vhs")
+public class VhsController {
+
+    // TODO: Implement
+    @GetMapping("/")
+    public String getAll() {
+        return "No VHS available right now.";
+    }
+
+    // TODO: Implement
+    @GetMapping("/{id}")
+    public String getById(@PathVariable long id) {
+        return "No VHS with " + id + " available right now.";
+    }
+
+    // TODO: Implement
+    @PostMapping("/")
+    public String create() {
+        return "Created a new tape";
+    }
+
+    // TODO: Implement
+    @PutMapping("/")
+    public String update() {
+        return "Updated a tape";
+    }
+
+    // TODO: Implement
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable long id) {
+        return "Deleted a tape";
+    }
+}
